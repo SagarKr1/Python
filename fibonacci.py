75017938030f284b6e847ad2e1724782e5935f62
SagarKumar977 <sag197348@gmail.com>
2025-02-17 15:18:13 +0530

    update

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,16 @@
 # 3. Write a Python function called fibonacci that takes an integer n .
 
-n1=0
-n2=1
-num = int(input("Enter n term: "))
-print(f"{n1} {n2}",end=" ")
-while num >0 :
-    sum = n1+n2
-    print(sum,end=" ")
-    n1 = n2
-    n2 = sum
-    num-=1
+def fibonacci(num):
+    n1=0
+    n2=1
+    print(f"{n1} {n2}",end=" ")
+    while num >0 :
+        sum = n1+n2
+        print(sum,end=" ")
+        n1 = n2
+        n2 = sum
+        num-=1
+
+n = int(input("Enter n term: "))
+
+fibonacci(n)
--- table.py
+++ table.py
@@ -4,5 +4,8 @@
 
 num = int(input("Enter a number: "))
 
-for i in range(1,11):
-    print(f"{num} X {i} = {num*i}")
+def Table(n):
+    for i in range(1,11):
+        print(f"{n} X {i} = {n*i}")
+        
+Table(num)
